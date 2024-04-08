e46e44be5a1c1d86c66d4c3db6018310c20b35f1
saidBaroudi <saeed.baroody@gmail.com>
2024-04-08 23:41:38 +0300

    map is running, borderlines are running

--- static/mapJS.js
+++ static/mapJS.js
@@ -1,7 +1,27 @@
-function initMap(){
-  var options = {
-    center: {lat: 37.7749, lang: -122.4194},
-    zoom: 8
-  }
-  map = new google.maps.Map(document.getElementById("map"), options);
+let map;
+let featureLayer;
+async function initMap() {
+  const {Map} = await google.maps.importLibrary("maps");
+  map = new Map(document.getElementById("map"), {
+    center: {lat: 41.1472, lng: 28.7844},
+    zoom: 10,// Adjust the zoom level as needed
+    mapId: "a449062d42e614ea"
+  });
+  featureLayer = map.getFeatureLayer("ADMINISTRATIVE_AREA_LEVEL_1");
+
+  const featureStyleOptions = {
+    strokeColor: "#353535",
+    strokeOpacity: 1.0,
+    strokeWeight: 3.0,
+    fillColor: "transparent",
+    fillOpacity: 0,
+  };
+
+  featureLayer.style = (options) => {
+    if (options.feature.placeId == "ChIJexVgWlG2yhQR6B1akfSarCI") {
+      return featureStyleOptions;
+    }
+  };
 }
+
+initMap().then(r => initMap());
